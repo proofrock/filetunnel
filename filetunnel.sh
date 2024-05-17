65cc9e4194acce4a3fece19dd97469015e29428a
Germano Rizzo <oss@germanorizzo.it>
2024-05-17 11:15:03 +0200

    Version labeling

--- filetunnel.sh
+++ filetunnel.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# filetunnel v0.1.0 https://github.com/proofrock/filetunnel
+# filetunnel v0.1.2 https://github.com/proofrock/filetunnel
 # Copyright (c) 2024- Germano Rizzo <oss AT germanorizzo DOT it>
 # See LICENSE file (MIT License)
 
